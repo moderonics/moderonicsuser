7ce016455cfc81e5cfac4e92b04b5941d0b5cb10
anilkumawatRelanto <anil.kumawat@relanto.ai>
2025-10-02 23:07:08 +0530

    card improved

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -4,7 +4,7 @@ function Card({ className, ...props }) {
   return (
     <div
       data-slot="card"
-      className={cn("bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm", className)}
+      className={cn("bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-0 shadow-sm", className)}
       {...props}
     />
   )
